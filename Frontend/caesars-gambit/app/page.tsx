189c64277ea0affafb5af5292cff9aa9b0fce81b
knoepsim <simon.info.jc@gmail.com>
2025-11-04 11:57:33 +0100

    frontend: init shadcn
    - remove everything
    - init nextjs with tailwind
    - init shadcn
    - add example component on example page

---
+++ Frontend/caesars-gambit/app/page.tsx
@@ -0,0 +1,23 @@
+import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
+import { BadgeCheckIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Item variant="outline" size="sm" asChild>
+        <a href="#">
+          <ItemMedia>
+            <BadgeCheckIcon className="size-5" />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>Test Item.</ItemTitle>
+          </ItemContent>
+          <ItemActions>
+            <ChevronRightIcon className="size-4" />
+          </ItemActions>
+        </a>
+      </Item>
+    </div>
+  );
+}
